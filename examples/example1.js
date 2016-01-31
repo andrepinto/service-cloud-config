a5b0efa9de0be6c6d7c6b43e5cc87316f545f99b
andrepinto <andre40826@gmail.com>
2016-01-31 18:00:31 +0000

    <refact> Code refactoring

--- examples/example1.js
+++ examples/example1.js
@@ -3,23 +3,27 @@ var ServiceCloudConfig = require('../lib/index');
 
 var config = new ServiceCloudConfig({
     service:'my-service',
-    adapter:'consul'
+    adapter:{
+        name:'consul',
+        connection:{
+            host: '192.168.99.100',
+            port: 8500
+        }
+    },
+    mergeWithConfigEnv:true
 });
 
 
-config.start({}, function(err, data){
-
-    config.getRemoteValue('my-service/config1',  function(err, data){
-        console.log(err, data);
-    });
+config.start( function(err, data){
 
     config.getData(function(err, data){
+       console.log(data);
+        console.log(config.get('database.ip'));
+    });
+
+    config.getRemoteValue('my-service/database/ip',  function(err, data){
         console.log(err, data);
     });
 
 });
 
-
-
-
-setTimeout(null, 5000);
